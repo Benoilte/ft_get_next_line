748ed27d9cb79d745f2586a2ab63390ffd87f3dc
Bebrandt <benoit.07@hotmail.com>
2023-11-03 10:26:55 +0100

    add lst function for GNL

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,84 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:57:30 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/11/02 11:57:53 by bebrandt         ###   ########.fr       */
+/*   Updated: 2023/11/03 10:26:16 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+Create a new t_gnl_lst struct with malloc. assign var member content with
+params 'content' and assign var member next to NULL
+*/
+t_gnl_lst	*ft_gnl_lstnew(void *str)
+{
+	t_gnl_lst	*new;
+
+	new = (t_gnl_lst *)malloc(sizeof(t_gnl_lst));
+	if (!new)
+		return ((void *)0);
+	new->str = str;
+	new->next = (void *)0;
+	return (new);
+}
+
+/*
+Count number of element of the linked list and return the value.
+*/
+int	ft_gnl_lstsize(t_gnl_lst *lst)
+{
+	int			i;
+	t_gnl_lst	*el_lst;
+
+	i = 0;
+	el_lst = lst;
+	while (el_lst)
+	{
+		el_lst = el_lst->next;
+		i++;
+	}
+	return (i);
+}
+
+/*
+Add new elem. at the end of the list and set new->next as null
+set new elem. as first elem. of the list if '*lst' is null.
+*/
+void	ft_gnl_lstadd_back(t_gnl_lst **lst, t_gnl_lst *new)
+{
+	t_gnl_lst	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = *lst;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+}
+
+/*
+Deletes and free the memory of the element passed as parameter 
+and all the following elements
+*/
+void	ft_gnl_lstclear(t_gnl_lst **lst, void (*del)(void *))
+{
+	t_gnl_lst	*next_el;
+
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		next_el = (*lst)->next;
+		free((*lst)->str);
+		free(*lst);
+		*lst = next_el;
+	}
+	free(*lst);
+	*lst = (void *)0;
+}
