46efc55b5f55eed499d685a81bf7d4de95a0b35e
benoit brandt <benoit.07@hotmail.com>
2024-01-18 15:30:11 +0100

    modified main.c to test program

--- main.c
+++ main.c
@@ -6,26 +6,26 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:58:52 by bebrandt          #+#    #+#             */
-/*   Updated: 2023/11/10 10:27:48 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/01/18 15:22:14 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		fd;
 	int		i;
 	char	*new_line;
 
-	fd = open("text.txt", O_RDONLY);
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
 	i = 0;
 	while (i != 1000)
 	{
 		new_line = get_next_line(fd);
 		if (new_line)
 		{
-			printf("\nget_next_line: %d \n", i + 1);
 			printf("%s", new_line);
 			free(new_line);
 		}
